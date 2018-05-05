0e982d3bf91ee4c82fc8fe2243241bab17a331d7
Sebast <shenaom@mail.com>
2018-05-04 22:41:39 -0500

    Mejorar funciones y preventDefault

--- Formulario/scripts.js
+++ Formulario/scripts.js
@@ -1,64 +1,67 @@
 var formulario = document.getElementById("formulario");
+var e = 0 
 
-
-var validarNombre = function(e){
+function validarNombre(){
     if (formulario[0].value == "") {
         alert("Ingrese su Nombre")
-        e.preventDefault()
+        e++
     }
 }
 
-var validarCorreo = function(e){
+function validarCorreo(){
     if (formulario[1].value == "") {
         alert("Ingrese su Correo")
-        e.preventDefault()
+        e++
     }
 }
 
-var validarTelefono = function(e){
+function validarTelefono(){
     if (formulario[2].value.length != "10" || !/^([0-9])*$/.test(formulario[2].value) ) {
         alert("El teléfono no es válido. Ingrese sólo 10 numeros")
-        e.preventDefault()
+        e++
     }
 }
 
-var validarPais = function (e) {
+function validarPais() {
     if (formulario[3].value == "") {
         alert("Seleccione un País de la lista.")
-        e.preventDefault()
+        e++
     }
 }
 
-var validarUser = function (e) {
+function validarUser() {
     if (formulario[4].value == "") {
         alert("Ingrese un usuario")
-        e.preventDefault()
+        e++
     } else if (formulario[4].value.length >= 25 || formulario[4].value.length < 5 ) {
         alert("El usuario debe tener de 5 a 25 caracteres")
-        e.preventDefault()
+        e++
     } else if (!/^\w/.test(formulario[4].value)){
         alert("Ingrese sólo caracteres alfanuméricos")
-        e.preventDefault()
+        e++
     }
 }
 
-var validarPass = function (e) {
+function validarPass() {
     if (formulario[5].value == "") {
         alert("Ingrese contraseña")
-        e.preventDefault()
+        e++
     } else if (formulario[5].value.length >= 20 || formulario[5].value.length < 5){
         alert("Ingrese una contraseña de 5 a 20 caracteres, incluyendo caracteres especiales")
-        e.preventDefault()
+        e++
     }
 }
 
-var validar= function(e){
-        validarNombre(e);
-        validarCorreo(e);
-        validarTelefono(e);
-        validarPais(e);
-        validarUser(e);
-        validarPass(e);
+function validar(){
+        validarNombre();
+        validarCorreo();
+        validarTelefono();
+        validarPais();
+        validarUser();
+        validarPass();
+        if (e>0){
+            event.preventDefault()
+        }
 }
 
 formulario.addEventListener("submit", validar)
